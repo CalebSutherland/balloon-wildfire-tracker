dda37fe180ba674cc42037281ecca4847276f77f
CalebSutherland <sutherlandcaleb12@gmail.com>
2025-08-27 18:28:59 -0700

    Add Mapbox example

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,5 +1,39 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
+
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 export default function Map() {
-  return <div></div>;
+  const mapContainerRef = useRef<HTMLDivElement>(null);
+  const mapRef = useRef<mapboxgl.Map | null>(null);
+
+  useEffect(() => {
+    const container = mapContainerRef.current;
+    if (!container) return;
+
+    mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
+
+    mapRef.current = new mapboxgl.Map({
+      container,
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+
+    const marker = new mapboxgl.Marker({ color: "red" })
+      .setLngLat([-74.5, 40]) // [longitude, latitude]
+      .setPopup(new mapboxgl.Popup().setText("Hello, this is a marker!")) // optional popup
+      .addTo(mapRef.current);
+
+    return () => {
+      mapRef.current?.remove();
+    };
+  }, []);
+
+  return (
+    <div
+      style={{ height: "100%", width: "100%" }}
+      ref={mapContainerRef}
+      className="map-container"
+    />
+  );
 }
