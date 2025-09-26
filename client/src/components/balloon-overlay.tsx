c75db29904dd2d768b71a31c56dbb8f3e2f4c37c
CalebSutherland <sutherlandcaleb12@gmail.com>
2025-09-26 15:00:45 -0700

    Finish balloon display

--- client/src/components/balloon-overlay.tsx
+++ client/src/components/balloon-overlay.tsx
@@ -20,54 +20,69 @@ export function BalloonOverlay({
   fireCounts,
 }: BalloonOverlayProps) {
   let currentBalloon;
+  let latitude;
+  let longitude;
+  let altitude;
+  let maxAlt;
+  let distance;
+  let speed;
+
   if (selectedBalloonIndex != null) {
     currentBalloon = balloons[hour][selectedBalloonIndex];
+
+    latitude = currentBalloon.lat.toFixed(2);
+    longitude = currentBalloon.lon.toFixed(2);
+    altitude = currentBalloon.alt.toFixed(2);
+    maxAlt = maxAltitudes[currentBalloon.index].toFixed(2);
+    distance = (distances[currentBalloon.index] / 1000).toFixed(0);
+    speed = (distances[currentBalloon.index] / 1000 / 24).toFixed(0);
   }
 
   return (
     <div className="overlay-wrapper">
-      <div className="balloon-icon-wrapper">
-        <BalloonIcon
-          color={currentBalloon ? "red" : "grey"}
-          index={currentBalloon ? currentBalloon.index : null}
-        />
+      <div className="overlay-inner">
+        <div className="balloon-icon-wrapper">
+          <BalloonIcon
+            color={currentBalloon ? "red" : "grey"}
+            index={currentBalloon ? currentBalloon.index : null}
+          />
+        </div>
+
+        {currentBalloon ? (
+          <div className="balloon-stats-wrapper">
+            <div className="stats-header">
+              <p>
+                <b>Fire Points Within 50km of Path</b>:{" "}
+                {fireCounts[currentBalloon.index]}
+              </p>
+            </div>
+            <div className="balloon-stats">
+              <p>
+                <b>Latitude</b>: {latitude}
+              </p>
+              <p>
+                <b>Longitude</b>: {longitude}
+              </p>
+              <p>
+                <b>Altitude</b>: {altitude} km
+              </p>
+              <p>
+                <b>Peak Altitude</b>: {maxAlt} km
+              </p>
+              <p>
+                <b>Distance Traveled</b>: {distance} km
+              </p>
+              <p>
+                <b>Average Speed</b>: {speed} km/h
+              </p>
+            </div>
+          </div>
+        ) : (
+          <div className="no-balloon">
+            <p>No Balloon Selected</p>
+          </div>
+        )}
       </div>
-
-      {currentBalloon ? (
-        <div>
-          <b>Balloon {selectedBalloonIndex}</b>
-          <hr />
-
-          <li>
-            <b>Latitude</b>: {currentBalloon.lat.toFixed(2)}
-          </li>
-          <li>
-            <b>Longitude</b>: {currentBalloon.lon.toFixed(2)}
-          </li>
-          <li>
-            <b>Altitude</b>: {currentBalloon.alt.toFixed(2)} km
-          </li>
-          <li>
-            <b>Distance Traveled</b>:{" "}
-            {(distances[currentBalloon.index] / 1000).toFixed(0)} km
-          </li>
-          <li>
-            <b>Average Speed</b>:{" "}
-            {(distances[currentBalloon.index] / 1000 / 24).toFixed(0)} km/h
-          </li>
-          <li>
-            <b>Highest Altitude</b>:{" "}
-            {maxAltitudes[currentBalloon.index].toFixed(2)} km
-          </li>
-          <li>
-            <b>Fire Points in Range</b>: {fireCounts[currentBalloon.index]}
-          </li>
-        </div>
-      ) : (
-        <div>
-          <p>No Balloon Selected</p>
-        </div>
-      )}
     </div>
   );
 }
